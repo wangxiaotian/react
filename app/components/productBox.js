8eac0a474143103a79ba37f49eae9010ce3e89c7
[kaiyang] <wangdongyang@8think.cn>
2017-07-31 13:20:56 +0800

    initReact

---
+++ app/components/productBox.js
@@ -0,0 +1,12 @@
+var React = requrie('react');
+var ProductBox = React.createClass({
+	render: function(){
+		return (
+			<div className = "productBox">
+				Hello World!
+			</div>
+			);
+	}
+})
+
+module.exports = ProductBox;
